184ebcf20cd368050845d2b1fe7591bf91f71592
derek.morash <derek.morash@gmail.com>
2018-03-15 16:44:12 -0300

    dumb test of getting themes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     # @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @themes = ShopifyAPI::Theme.all
+    @themes = ShopifyAPI::Themes.all
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
