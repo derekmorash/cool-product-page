6d8694cf43297be27929ec76ec698dcd6600ba1e
derek.morash <derek.morash@gmail.com>
2018-03-15 16:18:26 -0300

    Updating shopify app scope permissions

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.scope = "read_orders, read_products"
+  config.scope = "read_themes, write_themes, write_script_tags"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
