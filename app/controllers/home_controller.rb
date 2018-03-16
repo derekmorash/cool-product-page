08cf65de9e68ba80bfd207e408db618544b121ea
derek.morash <derek.morash@gmail.com>
2018-03-16 10:27:11 -0300

    another stupid test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    # @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @collections = ShopifyAPI::Product.find(:all, params: { limit: 10 }))
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    # @collections = ShopifyAPI::Product.find(:all, params: { limit: 10 }))
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
